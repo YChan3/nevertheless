143437a4896d462b94d299e1d6bfcf0489c80f03
ychan3 <ychan3@stuy.edu>
2018-12-19 19:11:54 -0500

    5

--- basic_server.c
+++ basic_server.c
@@ -9,7 +9,8 @@ int main() {
 
   from_client = server_handshake( &to_client );
   while(1){
-    if(read(from_client, input, BUFFER_SIZE) != -1);
-    printf("%d\n", strlen(input));
+    if(read(from_client, input, BUFFER_SIZE) != -1){
+      printf("%d\n", strlen(input));
+    }
   }
 }
