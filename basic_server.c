09552681ad63d4df259b899853eadc5da4e9e599
ychan3 <ychan3@stuy.edu>
2018-12-19 20:26:48 -0500

    14

--- basic_server.c
+++ basic_server.c
@@ -1,15 +1,22 @@
 #include "pipe_networking.h"
 
 
+static void sighandler(int signo){
+  if (signo == SIGINT) {
+    remove("WKP");
+    exit(0);
+  }
+}
+
 int main() {
 
   int to_client;
   int from_client;
   char input[BUFFER_SIZE];
 
-  from_client = server_handshake( &to_client );
+  signal(SIGINT,sighandler);
+
   while(1){
-    remove("WKP");
     if(read(from_client, input, BUFFER_SIZE)){
       printf("your input was %ld characters.\n", strlen(input));
     }
